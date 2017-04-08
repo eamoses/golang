dde84329be4d92222f23298044e05dc6eccb0c29
Emily <e.a.moses@gmail.com>
2017-04-08 12:16:17 -0700

    adding struct with outside package

---
+++ struct.go
@@ -0,0 +1,14 @@
+package main
+import (
+  "fmt"
+  "vertex"
+)
+
+func main() {
+  vert := vertex.Vertex{0,12,1}
+  // var vert vertex.Vertex
+  // vert.X = 0
+  // vert.Y = 12
+  // vert.Z = 1
+  fmt.Println(vert.X,vert.Y,vert.Z)
+}
