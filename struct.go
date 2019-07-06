a8f2fdd9ae3261b79e698eadc4b0331ddf78662f
eamoses <e.a.moses@gmail.com>
2019-07-06 18:29:01 -0500

    saving import package fix

--- struct.go
+++ struct.go
@@ -1,14 +1,16 @@
 package main
+
 import (
-  "fmt"
-  "vertex"
+	"fmt"
+
+	"github.com/EverythingMe/vertex"
 )
 
 func main() {
-  vert := vertex.Vertex{0,12,1}
-  // var vert vertex.Vertex
-  // vert.X = 0
-  // vert.Y = 12
-  // vert.Z = 1
-  fmt.Println(vert.X,vert.Y,vert.Z)
+	vert := vertex.Vertex{0, 12, 1}
+	// var vert vertex.Vertex
+	// vert.X = 0
+	// vert.Y = 12
+	// vert.Z = 1
+	fmt.Println(vert.X, vert.Y, vert.Z)
 }
